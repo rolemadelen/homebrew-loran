865ba39536e00a0c899c2e2b4116b66fcc6575c8
Jii Yoo <jiiyoo17@gmail.com>
2026-08-13 00:55:51 -0700

    v0.7.12 release

--- Casks/loran@0.7.12.rb
+++ Casks/loran@0.7.12.rb
@@ -1,16 +1,17 @@
-cask "loran" do
+cask "loran@0.7.12" do
   arch arm: "aarch64", intel: "intel"
 
   version "0.7.12"
   sha256 arm:   "ce10448ea7523005d22cb15d951909e12779a23f07bf775c13fb41e164b64757",
          intel: "2162a14fb441a93c1520bda757a63c714dd57050d9297e15500c3b96da2a8476"
 
-  url "https://gitlab.com/jiiyoo17/loran-releases/-/raw/main/releases/v0.7.0/loran-macosx-#{arch}-#{version}.dmg",
-      verified: "pub-8cba8c3991e24de0bb09ab4fc11e167b.r2.dev/loran/"
+  url "https://gitlab.com/jiiyoo17/loran-releases/-/raw/main/releases/v0.7.12/loran-macosx-#{arch}-#{version}.dmg"
   name "Loran"
   desc "Markdown note-taking app"
   homepage "https://loran.day/"
 
+  # Same app name/bundle as the main cask — only one can be installed at a time
+  conflicts_with cask: "loran"
   depends_on :macos
 
   app "loran.app"
