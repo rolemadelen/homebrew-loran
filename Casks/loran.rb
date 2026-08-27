6e89de321795508e9d5f622030be73760c928919
Jii Yoo <jiiyoo17@gmail.com>
2026-08-27 07:22:38 -0700

    v0.7.28 release

--- Casks/loran.rb
+++ Casks/loran.rb
@@ -2,8 +2,8 @@ cask "loran" do
   arch arm: "aarch64", intel: "intel"
 
   version "0.7.28"
-  sha256 arm:   "662e79a668e1de3bc94eea00a61a7ad00c6eb774e1b4565604939cd0b0e40503",
-         intel: "afb31b77d4ae1d3a31ba54301620a40120edbf55170b16c01b1a5b87a32debdd"
+  sha256 arm:   "7fd4299887a2ab6f1447c78bc206654624f02232699da8c923c6ec2bae2d153f",
+         intel: "737e3a8490dc7989089c9b57fc4ec7e0918a994bb7fcff23f8568048d0f75d80"
 
   url "https://gitlab.com/jiiyoo17/loran-releases/-/raw/main/releases/v#{version}/loran-macosx-#{arch}-#{version}.dmg",
       verified: "pub-8cba8c3991e24de0bb09ab4fc11e167b.r2.dev/loran/"
